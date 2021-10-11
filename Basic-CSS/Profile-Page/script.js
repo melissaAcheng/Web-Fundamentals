2037faac0092cffdb96fd9867b549e2b3d54938b
melissaAcheng <melcheng12@gmail.com>
2021-10-11 17:09:29 -0400

    connection requests and connections number adjusts based on clicking accept or deny button

--- Basic-CSS/Profile-Page/script.js
+++ Basic-CSS/Profile-Page/script.js
@@ -1,13 +1,19 @@
+var requestElement = document.querySelector("#circle");
+var connectionElement = document.querySelector("#oval");
+
 function confirm(element) {
   element.parentNode.remove();
+  requestElement.innerText--;
+  connectionElement.innerText++;
 }
 
 function deny(element) {
   element.parentNode.remove();
+  requestElement.innerText--;
 }
 
 function editProfile() {
   var newName = prompt("Enter full name");
-  var userName = document.getElementById("user-fullname");
-  userName.innerHTML = newName;
+  var userName = document.querySelector("#user-fullname");
+  userName.innerText = newName;
 }

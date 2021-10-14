9ddd09b2425e105455cf3c9fb47456ac09766cd6
melissaAcheng <melcheng12@gmail.com>
2021-10-14 10:43:22 -0400

    added js to change temperatures to celsius or farenheit

--- Basic-JavaScript/DojoWeather/script.js
+++ Basic-JavaScript/DojoWeather/script.js
@@ -3,3 +3,25 @@ var message = document.getElementById("message");
 function accept (el) {
   message.remove();
 }
+
+
+function c2f(temp) {
+  return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+  return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+  console.log(element.value);
+  for (let i=1; i<9; i++) {
+    var tempSpan = document.querySelector("#temp" + i);
+    var tempVal = tempSpan.innerText;
+    if(element.value == "°C") {
+      tempSpan.innerText = f2c(tempVal);
+    } else {
+      tempSpan.innerText = c2f(tempVal);
+    }
+  }
+}

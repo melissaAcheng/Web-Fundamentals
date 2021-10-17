85d5db106e5f54b947a8f7c85fc4552de8fca179
melissaAcheng <melcheng12@gmail.com>
2021-10-17 15:21:05 -0400

    for loop with array

---
+++ Basic-JavaScript/Array-Challenges/script.js
@@ -0,0 +1,12 @@
+function alwaysHungry(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "food") {
+      console.log("yummy");
+    } 
+  } console.log("I'm hungry");
+  }
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
